868fd34167438fd98cd47bdc5452b4b0e77d5c44
ran avital <ranavitaln@gmail.com>
2022-07-16 15:28:56 +0300

    initilaze

---
+++ TrickingLibary.Api/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrickingLibary.Api.Controllers
+{
+
+    [ApiController]
+    [Route("api/home")]
+    public class HomeController  : ControllerBase
+    {
+
+        [HttpGet]
+          public  string Index() {
+            return "Hello world";
+        }
+
+
+    }
+}
